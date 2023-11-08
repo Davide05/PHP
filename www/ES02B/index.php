aac4362f9dd814e7e88e537cdb3991d3cb7026b4
Davide05 <huzsdavide@gmail.com>
2023-11-08 10:00:35 +0000

    Update

--- www/ES02B/index.php
+++ www/ES02B/index.php
@@ -1,41 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Richiesta credenziali per l'accesso all'area riservata</title>
-</head>
-<body>
-
-<?php
-session_start();
-
-if (isset($_SESSION['user'])) {
-    echo "<p style='color: green;'>Benvenuto, {$_SESSION['user']}</p>";
-    echo "<a href='{$_SERVER['PHP_SELF']}?logout=true'>Logout</a>";
-} else {
-    $error_message = !empty($error) ? "<p style='color: red;'>$error</p>" : "";
-    echo "
-    <form method='POST' action='{$_SERVER['PHP_SELF']}'>
-        $error_message
-        <label for='username'>Username:</label>
-        <input type='text' name='username' id='username' required><br><br>
-
-        <label for='password'>Password:</label>
-        <input type='password' name='password' id='password' required><br><br>
-
-        <input type='submit' name='INVIO' value='Login'>
-    </form>";
-}
-?>
-
-</body>
-</html>
-
-
-<html>
 <head>
 	<title>Richiesta credenziali per l'accesso all'area riservata</title>
 </head>
 <body>
+<h1>Richiesta credenziali per l'accesso all'area riservata</h1>
+<h1>Home page - Davide Hu</h1><br><br>
+<h2>username = davide;<br>
+    password = capra;</h2><br><br>
 	<?php
 	if(isset($_POST["username"]) && isset($_POST["password"])) 
 	{
@@ -44,11 +14,11 @@ if (isset($_SESSION['user'])) {
 	
 		if($user!="admin"||$password!="password")
 		{
-			echo"<h4>nome utente o password sbagliate</h4>";
+			echo"<p style='color: red;'>Nome utente o password sbagliato</p>";
 		}
 		else
 		{
-			echo"<h4>benvenuto</h4>";
+			echo"<p style='color: green;'>Benvenuto .username.</p>";
 		}
 	}
 	else
